e0568a54d1dd58ba828d5c257f7ceacb21829715
OriTeicher <oriteicherudemy@gmail.com>
2024-06-21 12:13:25 +0300

    first commit

---
+++ force-app/main/default/lwc/taskManager/taskManager.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+import cmp from './cmps/animal.jsx'
+
+export default class TaskManager extends LightningElement {
+
+    connectedCallback(){
+        console.log('oriz')
+    }
+
+}
